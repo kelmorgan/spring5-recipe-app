7f08c7f6883c25219657ee016f61a645d0382b84
Kel Morgan <kelmorgan18@gmail.com>
2022-01-12 08:19:52 +0100

    update for recipe app

--- src/main/java/com/kelmorgan/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/com/kelmorgan/spring5recipeapp/controllers/IndexController.java
@@ -1,14 +1,33 @@
 package com.kelmorgan.spring5recipeapp.controllers;
 
+import com.kelmorgan.spring5recipeapp.domain.Category;
+import com.kelmorgan.spring5recipeapp.domain.UnitOfMeasure;
+import com.kelmorgan.spring5recipeapp.repositories.CategoryRepository;
+import com.kelmorgan.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"","/","/index"})
     public String getIndexPage(){
-        System.out.println("128w sw21rw s    asfasfasff  dwfwfrwf");
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Tablespoon");
+
+        System.out.println("Cat id is: "+ categoryOptional.get().getId());
+        System.out.println("unit id is: "+ unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
