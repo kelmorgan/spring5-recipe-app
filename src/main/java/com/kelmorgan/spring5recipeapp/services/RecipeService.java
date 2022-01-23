f768298f5dd8e6d63c338a1156c05dcf2bfaec68
Kel Morgan <kelmorgan18@gmail.com>
2022-01-23 08:57:17 +0100

    update for recipe app

--- src/main/java/com/kelmorgan/spring5recipeapp/services/RecipeService.java
+++ src/main/java/com/kelmorgan/spring5recipeapp/services/RecipeService.java
@@ -10,5 +10,10 @@ public interface RecipeService {
     Set<Recipe> getRecipes();
 
     Recipe findById(Long l);
+
     RecipeCommand saveRecipeCommand(RecipeCommand command);
+
+    RecipeCommand findCommandById(Long l);
+
+    void deleteById(Long idToDelete);
 }
